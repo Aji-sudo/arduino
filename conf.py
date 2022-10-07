c9db6610155b12869def0056a6fcc92a80631660
Aji-sudo <115223567+Aji-sudo@users.noreply.github.com>
2022-10-07 03:38:15 -0700

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,12 @@
+  GNU nano 6.2                                                                                          conf.py *                                                                                                  SID = 'AC927dc9d3e9d1f0d80f5fe6b164c80cca'
+AUTH_TOKEN = '037f9edf9493b0314b5f2b5a5d82f141'
+FROM_NUMBER = '+18645287307'
+TO_NUMBER = '+919866417851'
+API_KEY = '566afb1a-5d3e-4be1-83fb-7de03fb227e7'
+DEVICE_ID = 'BOLT8022598'
+
+
+
+
+
+
